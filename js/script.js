f3b8e82697192754649d4e48830f2785e9a7dd86
akarshit <akarshitsrivastava33@gmail.com>
2020-10-14 19:35:46 +0530

    completely done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 function validate(){
-    var a= document.getElementById("a1");
+    var a= document.getElementById("a1").value;
     var b= document.getElementById("b1");
-    var c1= document.getElementById("c1");
+    var c1= document.getElementById("phone").value;
     var d1= document.getElementById("d1");
-    if(a.value.trim()=="") {
-        alert("Enter the username");
-        a.style.border = "2px solid red";
-        //  document.getElementById("one").style.visibility = "visible";
-        return false;
+    var reg=  /^[a-zA-Z]+ [a-zA-Z]+$/
+    var regex= /^[7-9]\d{9}$/
+ if(reg.test(a)===false){
+        document.getElementById("use").innerHTML="Invalid PLease Check it."
+        document.getElementById("use").style.visibility="visible"
+        document.getElementById("use").style.color="red"
+        return false
+
     }
     else  if(b.value.trim()=="") {
         alert("Enter the password");
@@ -15,10 +18,12 @@ function validate(){
         //  document.getElementById("two").style.visibility = "visible";
         return false;
     }
-    else if (c1.value.trim()=="") {
-        c1.style.border = "2px solid red";
-        // document.getElementById("two").style.visibility = "visible";
-        return false;
+   else if(regex.test(c1)===false){
+        document.getElementById("ch").innerHTML="Invalid PLease Check it."
+        document.getElementById("ch").style.visibility="visible"
+        document.getElementById("ch").style.color="red"
+        return false
+
     }
     else if(d1.value.trim()==""){
         b.style.border = "2px solid red";
